e68d8242d531c63c2d2c63224f6276da3cd11cee
nickvanroyen <nick.vanroyen@ordina.be>
2024-09-30 13:51:41 +0200

    added first part

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to vacation Planner");
+        System.out.println("Welcome to vacation Planner!");
+        System.out.println("What is your name?");
+        String name = input.next();
+        System.out.println("Nice to meet you " + name + ", where are you travelling to?");
+        String travellingTo = input.next();
+        System.out.println("Great! " + travellingTo + " sounds like a great trip!");
+        System.out.println("********************");
     }
 }
