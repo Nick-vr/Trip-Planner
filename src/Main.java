f22007a686dbb91687d830a6df1f6cd783cd2316
nickvanroyen <nick.vanroyen@ordina.be>
2024-09-30 14:50:34 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a Scanner object to read input from the standard input stream (keyboard)
         Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to vacation Planner!");
+
         System.out.println("What is your name?");
-        String name = input.next();
+        String name = input.next(); // Read the user's name from the input
         System.out.println("Nice to meet you " + name + ", where are you travelling to?");
-        String travellingTo = input.next();
+
+        String travellingTo = input.next(); // Read the destination the user is travelling to from the input  
         System.out.println("Great! " + travellingTo + " sounds like a great trip!");
-        System.out.println("********************");
+
+        System.out.println("******************** \n");
+
+        System.out.println("How many days are you going to spend travelling?");
+        int amountOfDaysTravelling = input.nextInt();
+        int amountOfDaysTravellingInHours = TravelCalculations.daysIntoHours(amountOfDaysTravelling);
+        int amountOfDaysTravellingInMinutes = TravelCalculations.daysIntoMinutes(amountOfDaysTravelling);
+
+        System.out.println("How much money, in EUR, are you planning to spend on your trip?");
+        double amountOfMoneyToSpend = input.nextDouble();
+        double amountOfMoneyToSpendPerDay = amountOfMoneyToSpend / amountOfDaysTravelling;
+
+        System.out.println("What is the three letter currency symbol for your travel destination?");
+        String currency = input.next();
+
+        System.out.println("How many " + currency + " are there in 1 EUR?");
+        double currencyConversionAmount = input.nextDouble();
+        double euroToChosenCurrency = 1 * currencyConversionAmount;
+
+        System.out.println(
+                "If you are travelling for " + amountOfDaysTravelling +
+                " days that is the same as " + amountOfDaysTravellingInHours +
+                " hours or " + amountOfDaysTravellingInMinutes + " minutes");
+
+        System.out.println("If you are going to spend €" + amountOfMoneyToSpend +
+                " that means per day you can spend up to €" + amountOfMoneyToSpendPerDay);
+
+        System.out.println("Your total budget in " + currency + " is " + (euroToChosenCurrency * amountOfMoneyToSpend) + " ,which per day is " + (euroToChosenCurrency * amountOfMoneyToSpend / amountOfDaysTravelling));
+
+        System.out.println("******************** \n");
     }
 }
---
+++ src/TravelCalculations.java
@@ -0,0 +1,9 @@
+public class TravelCalculations {
+    public static int daysIntoHours(int arg) {
+        return arg * 24;
+    }
+
+    public static int daysIntoMinutes(int arg) {
+        return arg * 24 * 60;
+    }
+}
