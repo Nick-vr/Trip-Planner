5f0cc0a80c4a55b6b0eaf01ec570d97c93d9d027
nickvanroyen <nick.vanroyen@ordina.be>
2024-09-30 13:45:37 +0200

    added idea to gitignore

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Welcome to vacation Planner");
     }
 }
